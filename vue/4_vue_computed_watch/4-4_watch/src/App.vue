4e9ce8bc267323ee52507800a99d3b93ea2bc9b0
cookie0215 <jina_0215@naver.com>
2022-01-02 17:24:02 +0900

    feat: study vue_computed & watch

---
+++ vue/4_vue_computed_watch/4-4_watch/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1 @click="changeMsg">{{ msg }}</h1>
+    <h1>{{ reversedMsg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // data속성에 지정한 값은 기본적으로 getter와 setter용도 2가지로 사용할 수 있다.
+      msg: 'Hello~'
+    }
+  },
+  computed: {
+    reversedMsg() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+  // 변화를 감지하고 싶은 data속성을 함수형태로 작성하면 된다.
+  watch: {
+    msg() {
+      console.log(`msg: ${this.msg}`);
+    },
+    reversedMsg() {
+      console.log(`reversedMsg: ${this.reversedMsg}`);
+    }
+  },
+  methods: {
+    changeMsg() {
+      this.msg = 'Good!'
+    }
+  }
+}
+</script>
+
+<style>
+  
+</style>
